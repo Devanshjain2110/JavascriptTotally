f7f7a3df38402bcef4eac7d632c4bfcbd9bbf5bb
Devanshjain2110 <92254216+Devanshjain2110@users.noreply.github.com>
2023-12-25 20:43:57 +0530

    Made the again button work

--- Main/Beginner/Guess the Number/script.js
+++ Main/Beginner/Guess the Number/script.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 100) + 1
+let secretNumber = Math.trunc(Math.random() * 100) + 1
 let score = 10;
-console.log(secretNumber)
+
 document.querySelector('.check').addEventListener('click', function(){
    const guess =  Number(document.querySelector('.guess').value);
-   console.log(guess, typeof guess)
+
 
    // When there is no input
    if(!guess){
@@ -14,8 +14,10 @@ document.querySelector('.check').addEventListener('click', function(){
     // When Player wins
     else if(guess === secretNumber){
     document.querySelector('.message').textContent = '🎇 Correct Number!'
+    document.querySelector('.number').textContent = secretNumber
     document.querySelector('body').style.backgroundColor = '#60b347'
     document.querySelector('.number').style.width = '30rem'
+    
 }
     
     // When guess is too high
@@ -41,4 +43,17 @@ document.querySelector('.check').addEventListener('click', function(){
     }
   
    }
+})
+
+document.querySelector('.again').addEventListener('click', function(){
+    score = 10
+    secretNumber = Math.trunc(Math.random() * 100) + 1
+    document.querySelector('.message').textContent = 'Start guessing...'
+    document.querySelector('.score').textContent = score
+    document.querySelector('.guess').textContent = ''
+    document.querySelector('.number').textContent = '?'
+    document.querySelector('body').style.backgroundColor = '#222'
+    document.querySelector('.number').style.width = '15rem'
+   
+    
 })
